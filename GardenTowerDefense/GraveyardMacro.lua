33c30a330ea6ac2ccb709e63fe41674b49be7318
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2026-04-09 21:59:13 +0800

    Create GraveyardMacro.lua

---
+++ GardenTowerDefense/GraveyardMacro.lua
@@ -0,0 +1,93 @@
+return {
+    [20] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_log_roller";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 1,
+                Position = Vector3.new(-335, 61, -837),
+                DistanceAlongPath = 235,
+                CF = CFrame.new(-335, 61, -837, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "log_roller_1";
+        }
+    };
+    [40] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_log_roller";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 1,
+                Position = Vector3.new(-335, 61, -837),
+                DistanceAlongPath = 235,
+                CF = CFrame.new(-335, 61, -837, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "log_roller_2";
+        },{
+            ["Task"] = "Upgrade Unit";
+            ["Upgrades"] = 1;
+            ["ID"] = "log_roller_2";
+        }
+    },
+    [55] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_log_roller";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 1,
+                Position = Vector3.new(-335, 61, -840),
+                DistanceAlongPath = 235,
+                CF = CFrame.new(-335, 61, -837, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "log_roller_3";
+        },{
+            ["Task"] = "Upgrade Unit";
+            ["Upgrades"] = 2;
+            ["ID"] = "log_roller_3";
+        }
+    },
+    [70] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_log_roller";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 1,
+                Position = Vector3.new(-335, 61, -840),
+                DistanceAlongPath = 235,
+                CF = CFrame.new(-335, 61, -837, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "log_roller_4";
+        },{
+            ["Task"] = "Upgrade Unit";
+            ["Upgrades"] = 2;
+            ["ID"] = "log_roller_4";
+        }
+    },
+    [90] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_log_roller";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 1,
+                Position = Vector3.new(-335, 61, -840),
+                DistanceAlongPath = 235,
+                CF = CFrame.new(-335, 61, -837, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "log_roller_5";
+        },{
+            ["Task"] = "Upgrade Unit";
+            ["Upgrades"] = 2;
+            ["ID"] = "log_roller_5";
+        }
+    }
+}
