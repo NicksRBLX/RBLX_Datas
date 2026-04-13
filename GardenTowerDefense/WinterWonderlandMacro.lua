e3837892aa9a30a14d0546f8e067ad04ba3c3f94
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2026-04-13 09:33:55 +0800

    Update WinterWonderlandMacro.lua

--- GardenTowerDefense/WinterWonderlandMacro.lua
+++ GardenTowerDefense/WinterWonderlandMacro.lua
@@ -59,4 +59,71 @@ return {
             ["ID"] = "rafflesia_4";
         }
     };
+	[45] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_rafflesia";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 1,
+                Position = Vector3.new(109, 1, -96),
+                DistanceAlongPath = 5,
+                CF = CFrame.new(109, 1, -96, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "rafflesia_5";
+        }
+    };
+	[55] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_rafflesia";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 2,
+                Position = Vector3.new(112, 1, 109),
+                DistanceAlongPath = 5,
+                CF = CFrame.new(112, 1, 109, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "rafflesia_6";
+        };
+		{
+			["Task"] = "Sell Unit";
+			["ID"] = "rafflesia_1"
+		}
+    };
+	[65] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_rafflesia";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 3,
+                Position = Vector3.new(-104, 1, 91),
+                DistanceAlongPath = 5,
+                CF = CFrame.new(-104, 1, 91, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "rafflesia_7";
+        },{
+			["Task"] = "Sell Unit";
+			["ID"] = "rafflesia_2"
+		}
+    };
+	[75] = {
+        {
+            ["Task"] = "Place Unit";
+            ["Unit"] = "unit_rafflesia";
+            ["Data"] = {
+                Valid = true,
+                PathIndex = 4,
+                Position = Vector3.new(-82, 1, -124),
+                DistanceAlongPath = 5,
+                CF = CFrame.new(-82, 1, -124, -1, 0, -0, -0, 1, -0, -0, 0, -1),
+                Rotation = 180
+            };
+            ["ID"] = "rafflesia_8";
+        }
+    };
 }
