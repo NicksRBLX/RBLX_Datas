bdc65148c58d04d2a518e78767c5fbef35f38398
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2026-04-14 22:51:21 +0800

    Create DATA.lua

---
+++ GardenTowerDefense/DATA.lua
@@ -0,0 +1,23 @@
+return {
+	["EGGS"] = {
+		"beanstalk",
+		"bling",
+		"chocolate",
+		"conveyor",
+		"defeat",
+		"disco",
+		"endless",
+		"floral",
+		"flowering",
+		"greenhouse",
+		"haybale",
+		"leaderboard",
+		"rage",
+		"rainbow",
+		"rock",
+		"survival",
+		"tree",
+		"upgrade",
+		"windmill",
+	}
+}
