400cfa12914f116118491be41156634146755f5c
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2026-04-14 23:50:26 +0800

    Update DATA.lua

--- GardenTowerDefense/DATA.lua
+++ GardenTowerDefense/DATA.lua
@@ -22,16 +22,24 @@ return {
 		"windmill",
 	};
 	
-	["GNOME_SHOP"] = {
-		["Astral Summon"] = { ID = "ub_astral", PRICE_X1 = 300, PRICE_X10 = 2700 },
-		["Bee Summon"] = { ID = "ub_bee", PRICE_X1 = 300, PRICE_X10 = 2700 },
-		["Classic Summon"] = { ID = "ub_classic", PRICE_X1 = 100, PRICE_X10 = 900 },
-		["Corrupted Summon"] = { ID = "ub_corrupted", PRICE_X1 = 500, PRICE_X10 = 4500 },
-		["Crystal Summon"] = { ID = "ub_crystal", PRICE_X1 = 250, PRICE_X10 = 2250 },
-		["Enchanted Summon"] = { ID = "ub_enchanted", PRICE_X1 = 150, PRICE_X10 = 1350 },
-		["Greenhouse Summon"] = { ID = "ub_greenhouse", PRICE_X1 = 500, PRICE_X10 = 4500 },
-		["Mushroom Summon"] = { ID = "ub_mushroom", PRICE_X1 = 300, PRICE_X10 = 2700 },
-		["Sun Summon"] = { ID = "ub_sun", PRICE_X1 = 200, PRICE_X10 = 1800 },
-		["Tropical Summon"] = { ID = "ub_tropical", PRICE_X1 = 200, PRICE_X10 = 1800 },
+	["SUMMON"] = {
+		["SEEDS"] = {
+			["Astral Summon"] = { ID = "ub_astral", PRICE_X1 = 300, PRICE_X10 = 2700 },
+			["Bee Summon"] = { ID = "ub_bee", PRICE_X1 = 300, PRICE_X10 = 2700 },
+			["Classic Summon"] = { ID = "ub_classic", PRICE_X1 = 100, PRICE_X10 = 900 },
+			["Corrupted Summon"] = { ID = "ub_corrupted", PRICE_X1 = 500, PRICE_X10 = 4500 },
+			["Crystal Summon"] = { ID = "ub_crystal", PRICE_X1 = 250, PRICE_X10 = 2250 },
+			["Enchanted Summon"] = { ID = "ub_enchanted", PRICE_X1 = 150, PRICE_X10 = 1350 },
+			["Greenhouse Summon"] = { ID = "ub_greenhouse", PRICE_X1 = 500, PRICE_X10 = 4500 },
+			["Mushroom Summon"] = { ID = "ub_mushroom", PRICE_X1 = 300, PRICE_X10 = 2700 },
+			["Sun Summon"] = { ID = "ub_sun", PRICE_X1 = 200, PRICE_X10 = 1800 },
+			["Tropical Summon"] = { ID = "ub_tropical", PRICE_X1 = 200, PRICE_X10 = 1800 },
+		};
+		["GEMS"] = {
+			["Galaxy Summon"] = { ID = "ub_galaxy", PRICE_X1 = 25, PRICE_X10 = 225 },
+		};
+		["EGGS"] = {
+			["Bunny Summon"] = { ID = "ub_bunny", PRICE_X1 = 100, PRICE_X10 = 900 },
+		};
 	};
 }
